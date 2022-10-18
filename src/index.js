0a599074cf9e2e93036fea5ec52d017d31f3a8dd
RahinF <s3297352@student.rmit.edu.au>
2022-10-18 20:29:46 +1100

    add cors config and error handler middleware

--- src/index.js
+++ src/index.js
@@ -1,15 +1,23 @@
 import express from "express";
+import cors from 'cors'
+import cookieParser from "cookie-parser";
 import { logger } from "./middleware/logger.js";
+import errorHandler from "./middleware/errorHandler.js";
+import corsOptions from "./config/corsOptions.js";
 
 const app = express();
 const PORT = process.env.PORT || 3500;
 
 app.use(logger);
+app.use(cors(corsOptions));
 app.use(express.json());
+app.use(cookieParser);
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.get("/", (request, response) => {
+  response.send("Hello World!");
 });
+
+app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
---
+++ src/middleware/errorHandler.js
@@ -0,0 +1,18 @@
+import { logEvents } from "./logger.js";
+
+const errorHandler = (error, request, response, next) => {
+  logEvents(
+    `${error.name}: ${error.message}\t${request.method}\t${request.url}\t${request.headers.origin}`,
+    `errorLog.log`
+  );
+
+  console.log(error.stack);
+
+  const status = response.statusCode ? response.statusCode : 500;
+
+  response.status(status);
+  response.json({ message: error.message });
+};
+
+
+export default errorHandler;
